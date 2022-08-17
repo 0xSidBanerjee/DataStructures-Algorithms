c6e78cb3dfeecca8d146a191c55fa081dbced03a
Sidoryx <siddhantban02@gmail.com>
2022-08-18 00:23:25 +0530

    Agnostic Binary Search :D

--- Binary Search/binarySalgo.java
+++ Binary Search/binarySalgo.java
@@ -1,16 +1,23 @@
-public class Test {
+import java.util.Scanner;
+
+public class binarySalgo {
     public static void main(String[] args) {
         int[] arr = { 10, 9, 8, 7, 5, 4, 3, 2, 1, 0 };
-        int target = 6;
-        System.out.println(search(arr, target));
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the target number");
+        int target = in.nextInt();
+        int result = search(arr, target);
+        if (result == -1) {
+            System.out.println("Number not found");
+        } else {
+            System.out.println("Number found at index " + result);
+        }
     }
 
     static int search(int[] arr, int target) {
         int start = 0;
         int end = arr.length - 1;
         int flag = 0;
-
-        // For descending order
         if (arr[0] > arr[arr.length - 1]) {
             flag = 1;
             int[] arr2 = new int[arr.length];
@@ -21,21 +28,21 @@ public class Test {
             temp = arr2;
             arr2 = arr;
             arr = temp;
-        }
 
-        int mid = start + (end - start) / 2;
-        while (start <= end) { // Why <= is required and not just < ?
-            mid = start + (end - start) / 2;
-            if (target < arr[mid]) {
-                end = mid - 1;
-            } else if (target > arr[mid]) {
+        }
+        int mid = start + (end - start) / 2; // implement this after checking order agnostic binary search
+        while (start <= end) {
+            if (arr[mid] > target) {
                 start = mid + 1;
+            } else if (arr[mid] < target) {
+                end = mid - 1;
             } else {
                 if (flag == 1) {
-                    return arr.length - 1 - mid;
+                    return arr.length - mid - 1;
                 }
                 return mid;
             }
+            mid = start + (end - start) / 2;
         }
         return -1;
     }
